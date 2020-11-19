2821c306de2aaac90287d2cc151addb4a569961f
Vivioza5 <VItaliyvivian12@gmail.com>
2020-11-19 21:25:13 +0200

    firsting

---
+++ python_course.py
@@ -0,0 +1,45 @@
+# i = 7
+# a = 5
+# while i < 11:
+#     a += i
+#     i += 2
+# print(a)
+
+
+# num = int(input())
+# has_seven = False  # сигнальная метка
+#
+# while num != 0:
+#     last_digit = num % 10
+#     if last_digit == 7:
+#         has_seven = True
+#     num = num // 10
+#
+# if has_seven == True:
+#     print('YES')
+# else:
+#     print('NO')
+# num = 12345
+# product = 1
+# while num != 0:
+#     last_digit = num % 10
+#     product = product * last_digit
+#     num = num // 10
+# print(product)
+
+num = 8945
+last_digit = num % 10
+min=last_digit
+max=last_digit
+while num != 0:
+    last_digit = num % 10
+    if min>last_digit :
+        min=last_digit
+    else:
+        max=last_digit
+    num = num // 10
+print(min)
+print(max)
+
+
+
