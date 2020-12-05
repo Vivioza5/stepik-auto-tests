41b6b587018d2ed906cbb6382eef7fcdec1deda6
Vivioza5 <VItaliyvivian12@gmail.com>
2020-12-05 15:40:48 +0200

    first

--- python_course.py
+++ python_course.py
@@ -393,5 +393,8 @@
 #     print(fib)
 #     first=second
 #     second=fib
-s = 'abcdefg'
-print(s[0]*3 + s[-1]*3 + s[3]*2 + s[3]*2)
+# s = 'abcdefg'
+# print(s[0]*3 + s[-1]*3 + s[3]*2 + s[3]*2)
+s = '01234567891011121314151617'
+for i in range(0, len(s), 5):
+    print(s[i], end='')
