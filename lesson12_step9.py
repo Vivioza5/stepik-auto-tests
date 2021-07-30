3eace9efb714088fa8c60c1363f9511562c99891
Vitaliy <scromnyagga@gmail.com>
2021-07-30 19:43:10 +0300

    today2

--- lesson12_step9.py
+++ lesson12_step9.py
@@ -2,11 +2,13 @@ import unittest
 from selenium import webdriver
 import time
 from pathDriver import path_driver
+
 link = "http://suninjuly.github.io/registration1.html"
 link1 = "http://suninjuly.github.io/registration2.html"
 
 
 def action_on_page(link):
+    browser = webdriver.Chrome(path_driver)
     browser.get(link)
     input1 = browser.find_element_by_css_selector("div.first_block input.form-control.first")
     input1.send_keys("Ivan")
@@ -14,26 +16,25 @@ def action_on_page(link):
     input2.send_keys("Petrov")
     input3 = browser.find_element_by_css_selector("div.first_block input.form-control.third")
     input3.send_keys("Petrov@gmail.com")
-    # input3 = browser.find_element_by_class_name("form-control.city")
-    # input3.send_keys("Smolensk")
-    # input4 = browser.find_element_by_id("country")
-    # input4.send_keys("Russia")
+    input4 = browser.find_element_by_css_selector("div.second_block input.form-control.first")
+    input4.send_keys("0986134770")
+    input5 = browser.find_element_by_css_selector("div.second_block input.form-control.second")
+    input5.send_keys("Smolensk")
     button = browser.find_element_by_css_selector("button.btn")
     button.click()
     time.sleep(1)
     welcome_text_elt = browser.find_element_by_tag_name("h1")
     welcome_text = welcome_text_elt.text
+    time.sleep(10)
     print(welcome_text)
     return welcome_text
 
 
-try:
-    browser = webdriver.Chrome(path_driver)
-    page1 = action_on_page(link)
-    # page2 = action_on_page(link1)
-    print(page1)
-
-#     # browser.get("http://suninjuly.github.io/registration1.html")
+# try:
+#     browser = webdriver.Chrome(path_driver)
+#     page1 = action_on_page(link)
+#     page2 = action_on_page(link1)
+# browser.get("http://suninjuly.github.io/registration1.html")
 #     # input1 = browser.find_element_by_css_selector("div.first_block input.form-control.first")
 #     # input1.send_keys("Ivan")
 #     # input2 = browser.find_element_by_css_selector("div.first_block input.form-control.second")
@@ -52,24 +53,24 @@ try:
 #     #
 #     # print(welcome_text)
 #     # assert "Congratulations! You have successfully registered!" == welcome_text
-finally:
-# успеваем скопировать код за 30 секунд
-    time.sleep(10)
-# закрываем браузер после всех манипуляций
-    browser.quit()
+# finally:
+# # успеваем скопировать код за 30 секунд
+#     time.sleep(10)
+# # закрываем браузер после всех манипуляций
+#     browser.quit()
 
 
 # не забываем оставить пустую строку в конце файла
 class TestAbs(unittest.TestCase):
     def test_welcome_text(self):
-        self.assertEqual("Congratulations! You have successfully registered!", page1,
-                         "Should be absolute value of a number")
+        # page1 = action_on_page(link1)
+        # self.assertEqual(page1, "Congratulations! You have successfully registered!", "not registered")
+        page2 = action_on_page(link1)
+        self.assertEqual(page2, "Congratulations! You have successfully registered!", "not registered")
 
-    # def test_abs2(self):
-    #     self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
+
 
 
 if __name__ == "__main__":
     unittest.main()
-    TestAbs.test_welcome_text(page1)
-    # TestAbs.test_welcome_text(page2)
+    TestAbs.test_welcome_text()
